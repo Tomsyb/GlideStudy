9057dee6b70b52ebca4baef51c7473293519291d
yanbo <760375443@qq.com>
2018-02-24 15:15:17 +0800

    readMe测试2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Android图片加载框架Glide最全解析（将会包括Glide的基本用法、源码解析、高级用法、功能扩展等内容）
 
-# 开始
+# 配置
 
 使用3.7.0稳定版，
 
@@ -11,4 +11,27 @@ dependencies {
     compile 'com.github.bumptech.glide:glide:3.7.0'
 }
 ```
+## 权限
+AndroidManifest.xml加
+
+```
+<uses-permission android:name="android.permission.INTERNET" />
+```
+ ## 普通加载
+
+```
+Glide.with(this)
+                .load(imgUrl)
+                .into(mImg);
+```
+
+解析上面加载
+
+- ** with **:  创建加载图片的实例可接Activity，Fragment，Context，前两种可直接传this,
+但是如果没在Activity或Fragment中，我们可以传ApplicationContext，传入的决定图片加载的生命周期
+前两个是和Activity和Fragment被销毁图片加载停止，ApplicationContext应用程序被杀掉的时候图片加载才停止
+
+- ** load() **:
+图片资源（网络图片，本地图片，应用资源,二进制流,Uri对象等）
+
 
--- app/src/main/java/com/android/daqsoft/glidestudy/MainActivity.java
+++ app/src/main/java/com/android/daqsoft/glidestudy/MainActivity.java
@@ -2,12 +2,43 @@ package com.android.daqsoft.glidestudy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView mImg;
+    private Button btn;
+    private String imgUrl = "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3736398904,3705920538&fm=27&gp=0.jpg";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mImg = findViewById(R.id.img);
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                method1();
+            }
+        });
+    }
+
+    /**
+     * 加载图片
+     */
+    public void method1(){
+        Glide.with(this)
+                .load(imgUrl)
+                .into(mImg);
     }
 }
+
+
+
